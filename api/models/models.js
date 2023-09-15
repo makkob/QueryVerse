3a4d393c4e58746c036a4d5148a9df3de746cf8e
makkob <maximkabig945@gmail.com>
2023-09-15 20:26:17 +0300

    app

--- api/models/models.js
+++ api/models/models.js
@@ -6,6 +6,8 @@ const User = sequelize.define("user", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   email: { type: DataTypes.STRING, unique: true },
   password: { type: DataTypes.STRING },
+  isActivated:{type:DataTypes.BOOLEAN},
+  activationLink:{type:DataTypes.STRING},
   phone: { type: DataTypes.STRING },
   firstName: { type: DataTypes.STRING },
   secondName: { type: DataTypes.STRING },
@@ -13,6 +15,12 @@ const User = sequelize.define("user", {
   likes: { type: DataTypes.TINYINT, defaultValue: 0 },
   disLikes: { type: DataTypes.TINYINT, defaultValue: 0 },
   role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
+
+const Token = sequelize.define("Token", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  userId: { type: DataTypes.INTEGER, allowNull: false }, // Ссылка на пользователя, которому принадлежит токен
+  refreshToken: { type: DataTypes.STRING, allowNull: false },
 });
 
 const Wall = sequelize.define("wall", {
@@ -51,10 +59,15 @@ PostsOnTheWall.belongsTo(Post);
 Post.hasMany(CommentsBelowThePost, { as: "commentsBelowThePost" });
 CommentsBelowThePost.belongsTo(Post);
 
+// Устанавливаем связь между моделями User и Token
+User.hasMany(Token, { foreignKey: 'userId' });
+Token.belongsTo(User, { foreignKey: 'userId' });
+
 module.exports = {
   User,
   Wall,
   PostsOnTheWall,
   Post,
   CommentsBelowThePost,
+  Token,
 };
--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -5,7 +5,16 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/registration", userController.registration);
 router.post("/login", userController.login);
-// router.get('/auth', authMiddleware, userController.check);
-// router.post('/edit/:userId', authMiddleware, userController.editUserInfo);
+
+// router.post('/logout', userController.logout);
+
+//для активации акаунта по ссылке которая будет приходить по почте
+// router.get('/activate/:link', userController.activate);
+
+//перезапись access токена если он умер
+// router.get('/refresh', userController.refresh);
+
+//  список всех юзеров
+// router.get('/users', authMiddleware, userController.getUsers);
 
 module.exports = router;
